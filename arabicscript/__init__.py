da05a078cd0fcb9ae81a81c4c27450e7e1267615
Abdul Rahim Nizamani <abdulrahimnizamani@gmail.com>
2016-08-09 11:02:09 +0200

    Update __init__

--- arabicscript/__init__.py
+++ arabicscript/__init__.py
@@ -1,1 +1,6 @@
+from .Char import Char
+from .ArabChar import ArabChar
+from .ArabStr import ArabStr
+from .Joining import JoiningGroup, JoiningType
 
+__all__ = [Char, ArabChar, ArabStr, JoiningGroup, JoiningType]
